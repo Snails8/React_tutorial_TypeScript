5424bb77950b735ce6c22e8f5aa5d0b06a8a6c4d
Suzuki Kaito <tanichi.u.n.20@gmail.com>
2021-04-03 21:50:59 +0900

    square作成

--- src/index.tsx
+++ src/index.tsx
@@ -4,3 +4,14 @@ import './index.css';
 import { type } from 'os';
 
 type SquareState = 'O' | 'X' | null
+
+type SquareProps = {
+  value: SquareState
+  onClick: () => void
+}
+
+const Square = (props: SquareProps) => (
+  <button className='square' onClick={props.onClick}>
+    {props.value}
+  </button>
+)
